a57b52ce27cfb0cfda418b0246d839e76f75063a
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2024-06-10 12:10:34 -0300

    mudanças

--- Java/compile_all.py
+++ Java/compile_all.py
@@ -1,53 +1,49 @@
-import sys, os
-from subprocess import call, check_output, Popen, PIPE
+import sys
+import os
+from subprocess import Popen, PIPE, call
+from lazyme.string import color_print
 
 path = '.'
 action = 'compile'
 
 def file_exists(file_path):
-    if not file_path:
-        return False
-    else:
-        return os.path.isfile(file_path)
+    return os.path.isfile(file_path) if file_path else False
 
-def main():
-  for root, dirs, files in os.walk(path):
-    print ('Checking ' + root)
+def execute_command(root, action):
     makefile = os.path.join(root, "Makefile")
     if file_exists(makefile):
-      cmd = 'cd ' + root + '; make ' + action
-      #cmd = 'ls -la'
-      pipes = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-      std_out, std_err = pipes.communicate()
-      
-      if (action == 'compile') | (action == 'run'):
-        if pipes.returncode != 0:
-          # an error happened!
-          err_msg = "%s. Code: %s" % (std_err.strip(), pipes.returncode)
-          print ('[E] Error on ' + root + ': ')
-          print (err_msg)
-        elif len(std_err):
-          # return code is 0 (no error), but we may want to
-          # do something with the info on std_err
-          # i.e. logger.warning(std_err)
-          print ('[OK]')
-        else:
-          print ('[OK]')
-      if action == 'measure':
-        call(['sleep', '5'])
+        cmd = f'cd {root}; make {action}'
+        process = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        std_out, std_err = process.communicate()
+        
+        if action in {'compile', 'run'}:
+            if process.returncode != 0:
+                err_msg = f"{std_err.strip().decode()}. Code: {process.returncode}"
+                color_print(f'[E] Error on {root}: ', color='red', bold=True)
+                print(err_msg)
+            elif std_err:
+                color_print('[OK]', color='green')
+            else:
+                color_print('[OK]', color='green')
+        
+        if action == 'measure':
+            call(['sleep', '5'])
+
+def main():
+    for root, dirs, files in os.walk(path):
+        print(f'Checking {root}')
+        execute_command(root, action)
 
 if __name__ == '__main__':
-  if len(sys.argv) == 2:
-    act = sys.argv[1]
-    if (act == 'compile') | (act == 'run') | (act == 'clean') | (act == 'mem') | (act == 'measure'):
-      print ('Performing \"' + act + '\" action...')
-      action = act
+    if len(sys.argv) == 2:
+        act = sys.argv[1]
+        if act in {'compile', 'run', 'clean', 'measure'}:
+            color_print(f'Performing "{act}" action...', color='yellow', bold=True)
+            action = act
+        else:
+            color_print(f'Error: Unrecognized action "{act}"', color='red')
+            sys.exit(1)
     else:
-      print ('Error: Unrecognized action \"' + act + '\"')
-      sys.exit(1)
-  else:
-    print ('Performing \"compile\" action...')
-    action = 'compile'
-  
-  main()
-  
+        color_print('Performing "compile" action...', color='yellow', bold=True)
+
+    main()
